daacd81d681897e351531d2ce910fc6b8aded325
SiphoC <devwork90@gmail.com>
2025-10-05 14:22:53 +0200

    Initial front-end project commit

---
+++ Frontend/e-gawulo/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>hello </h1>
+      <img src={assets.gs_logo} alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
