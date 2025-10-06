6ed9e432757a522d449a700de363f092ea7fb990
SiphoC <devwork90@gmail.com>
2025-10-06 02:42:36 +0200

    feat: add completed static navbar with logo, navigation links, search, cart, and sign-in button

--- Frontend/e-gawulo/src/components/Navbar/Navbar.jsx
+++ Frontend/e-gawulo/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,46 @@
-import React from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
-      <h1>hello </h1>
-      <img src={assets.logo} alt="" />
+      <img src={assets.elogo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="search icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="basket icon" />
+          <div className="dot"></div>
+        </div>
+        <button>sing in</button>
+      </div>
     </div>
   );
 };
---
+++ Frontend/e-gawulo/src/pages/home/home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./home.css";
+const home = () => {
+  return <div></div>;
+};
+
+export default home;
