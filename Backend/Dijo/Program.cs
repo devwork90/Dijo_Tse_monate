aae5e7e3796f3e73c1d37c2d8dd0f37d1a0e5258
unknown <devwork90@gmail.com>
2026-05-04 22:52:21 +0200

    Updated docker_setup_configuration branch to be docker ready pointing to development

--- Backend/Dijo/Program.cs
+++ Backend/Dijo/Program.cs
@@ -123,7 +123,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
-                "https://localhost:5173"
+                "https://localhost:5173",
+                "http://localhost:3000"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
